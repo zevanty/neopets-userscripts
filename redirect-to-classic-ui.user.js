0423d3117091e0874f06667dfda10e21dc1e31d8
zevanty <95801+zevanty@users.noreply.github.com>
2024-05-01 23:58:25 -0700

    Redirection: fix Kreludor HTML map

--- redirect-to-classic-ui.user.js
+++ redirect-to-classic-ui.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Redirect to Classic UI
-// @version      1.0.1
+// @version      1.0.2
 // @author       zevanty
 // @description  Redirect the page to use classic UI. Note that some pages require Flash for it to work.
 // @include      /^https?:\/\/www\.neopets\.com\/(explore|trudys_surprise|market_plaza|market_bazaar)\.phtml$/
@@ -12,7 +12,7 @@
 // @include      /^https?:\/\/www\.neopets\.com\/jelly\/(index|jelly)\.phtml$/
 // @include      /^https?:\/\/www\.neopets\.com\/magma\/(index|caves)\.phtml$/
 // @include      /^https?:\/\/www\.neopets\.com\/medieval\/(index|index_castle|index_evil|index_farm|brightvale)\.phtml$/
-// @include      /^https?:\/\/www\.neopets\.com\/moon\/(index)\.phtml$/
+// @include      /^https?:\/\/www\.neopets\.com\/moon\/(index)\.phtml\/?$/
 // @include      /^https?:\/\/www\.neopets\.com\/pirates\/(index|warfwharf|anchormanagement)\.phtml$/
 // @include      /^https?:\/\/www\.neopets\.com\/prehistoric\/(index|plateau)\.phtml$/
 // @include      /^https?:\/\/www\.neopets\.com\/water\/(index|index_ruins|fishing)\.phtml$/
@@ -24,12 +24,21 @@
 (function() {
     'use strict';
 
-    // This may not work on certain browsers
-    location.href = location + "/";
-
-    // Backup in case the previous doesn't work
-    window.onload = function() {
-       location.href = location + "/";
+    // Kreludor HTML map no longer exist on Neopets server
+    if (location.toString().endsWith('/moon/index.phtml/')) {
+        // Alternative location: https://www.jellyneo.net/images/tourguide/kreludor_2005_01.gif
+        document.querySelector('img[usemap="#moon"]').setAttribute('src', 'https://zevanty.github.io/neopets-userscripts/assets/kreludor_2005_01.gif');
     }
+    // Perform the redirect
+    else {
+        // This may not work on certain browsers
+        location.href = location + "/";
+
+        // Backup in case the previous doesn't work
+        window.onload = function() {
+            location.href = location + "/";
+        }
+    }
+
 
 })();
