d7ef0408ab68440b5638fe4a637fb5747244dacb
zevanty <95801+zevanty@users.noreply.github.com>
2025-02-09 11:38:53 -0800

    Redirect "main" page to the "home" page

--- redirect-to-classic-ui.user.js
+++ redirect-to-classic-ui.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name         Redirect to Classic UI
-// @version      1.0.5
+// @version      1.0.6
 // @author       zevanty
 // @description  Redirect the page to use classic UI. Note that some pages require Flash for it to work.
+// @include      /^https?:\/\/www.neopets.com\/?$/
 // @include      /^https?:\/\/www\.neopets\.com\/(trudys_surprise|nf|market_plaza|market_bazaar)\.phtml$/
 // @include      /^https?:\/\/www\.neopets\.com\/(explore|generalstore)\.phtml\/?$/
 // @include      /^https?:\/\/www\.neopets\.com\/altador\/(index)\.phtml$/
@@ -79,6 +80,21 @@
         });
     }
 
+    // Skip the "main" page ("World of Neopets") and go directly to the "home" page
+    else if (currUrl.match(/^https?:\/\/www.neopets.com\/?$/)) {
+        let additionalSlash = '';
+        if (!currUrl.endsWith('/')) {
+            additionalSlash = '/';
+        }
+
+        // This may not work on certain browsers
+        location.href = location + additionalSlash + "home/";
+
+        // Backup in case the previous doesn't work
+        window.onload = function() {
+            location.href = location + additionalSlash + "home/";
+        }
+    }
 
     // Perform the redirect
     else {
